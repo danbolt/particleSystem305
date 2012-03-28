faa06a88e32ea5e09a6d26a63508b2590fa8429e
Daniel Savage <dansvg@gmail.com>
2012-03-28 12:06:54 -0700

    displays a simple interpolated square

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 
 SDL_Surface* screen;
 
+Uint8* keys;
+
 // initalization function for things like SDL and OpenGL
 int init()
 {
@@ -18,19 +20,67 @@ int init()
 		return 0;
 	}
 
-	if ((screen = SDL_SetVideoMode(320, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_OPENGL)) == NULL)
+	if ((screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_OPENGL)) == NULL)
 	{
 		perror("Error initalizing screen");
 		return 0;
 	}
 
 	SDL_WM_SetCaption("Particle System", NULL);
+	
+	glViewport(0, 0, 640, 480);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glShadeModel(GL_SMOOTH);
 	glEnable(GL_DEPTH_TEST);
+	
+	glOrtho(0, 640, 480, 0, 1, -1);
+	glMatrixMode(GL_MODELVIEW);
 
 	return 1;
+}
+
+void draw()
+{
+	glClearColor(0.0,0.0,0.0,1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glLoadIdentity();
+
+	glBegin(GL_QUADS);
+        glColor3f(1, 0, 0); glVertex3f(0, 0, 0);
+        glColor3f(1, 1, 0); glVertex3f(100, 0, 0);
+        glColor3f(1, 0, 1); glVertex3f(100, 100, 0);
+        glColor3f(1, 1, 1); glVertex3f(0, 100, 0);
+	glEnd();
+
+	SDL_GL_SwapBuffers();
+}
+
+void loop()
+{
+	int quit = 0;
+	SDL_Event ev;
+
+	while (!quit)
+	{
+		while(SDL_PollEvent(&ev))
+		{
+			if (ev.type == SDL_QUIT)
+			{
+				quit = 1;
+			}
+		}
+		
+		keys = SDL_GetKeyState(NULL);
+		
+		draw();
+	}
+
+	SDL_Delay(20);
 }
 
 int deinit()
@@ -45,7 +95,7 @@ int main (int argc, char* argv[])
 {
 	init();
 	
-	//
+	loop();
 	
 	deinit();
 	
